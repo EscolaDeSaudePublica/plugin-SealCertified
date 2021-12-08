473a27cbf3473445f8697d760f043ccf8bff4db1
FernandaNascimento26 <nandinha.cefet@hotmail.com>
2021-12-08 14:32:22 -0300

    Melhorias no layout para exibir nenhuma, uma ou duas assinaturas

--- layouts/parts/sealcertified/header.php
+++ layouts/parts/sealcertified/header.php
@@ -1,20 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
-<table width="100%" style="height: 100px;">
-    <thead>
-        <tr class="">
-            <td>                   
-                <img src="<?php echo PLUGINS_PATH.'PDFReport/assets/img/logo-saude.png'; ?>" style="float:left;"/>
-            </td>
-            <td>
-            <img src="<?php echo PLUGINS_PATH.'PDFReport/assets/img/ESP-CE-ORGAO-SEC-INVERTIDA-WEB2_3.png'; ?>" style="margin-left: 300px;" alt="">
-            </td>
-        </tr>
-    </thead>
-</table>
+<div height="100%">
+    <div width="100%" style="display: flex;">
+        <img src="<?php echo PLUGINS_PATH . 'PDFReport/assets/img/logo-saude.png'; ?>" class="img-logo-saude" style="margin-left: 24px;" />
+
+        <img src="<?php echo PLUGINS_PATH . 'PDFReport/assets/img/ESP-CE-ORGAO-SEC-INVERTIDA-WEB2_3.png'; ?>" class="img-logo-esp" />
+
+    </div>
