ad8aa70909fab1dc6719f9ee99383f147fdb68d4
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2022-05-10 11:42:56 -0300

    #10 Adiciona novo template no term db-updates migrations

--- db-updates.php
+++ db-updates.php
@@ -7,5 +7,8 @@ return array(
     'template certificados' => function () use($conn){
         $conn->executeQuery("INSERT INTO public.term (taxonomy, term, description) VALUES('seal_layout', 'template_certificado_padrao' , 'Certificado padrão da ESP');");
         $conn->executeQuery("INSERT INTO public.term (taxonomy, term, description) VALUES('seal_layout', 'template_declaracao_cuidar_melhor' , 'Certificado padrão do Cuidar Melhor');");
+    },
+    'add new template certificate default' => function () use($conn){
+        $conn->executeQuery("INSERT INTO public.term (taxonomy, term, description) VALUES('seal_layout', 'template_certificado_padrao_novo' , 'Novo Certificado padrão da ESP  (2022)');");
     }
 );
